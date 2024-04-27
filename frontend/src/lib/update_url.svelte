a0f0e408507e247215825fed5fcf3158b40b1b14
Theophilus <theophilus.ogbolu@gmail.com>
2024-04-27 17:01:34 +0100

    fixed url update

--- frontend/src/lib/update_url.svelte
+++ frontend/src/lib/update_url.svelte
@@ -3,9 +3,14 @@
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 
+	console.log($page.data.page_name);
+
 	onMount(() => {
-		if ($state[$page.data.page_name]) {
-			for (const x of new URLSearchParams($state[$page.data.page_name])) {
+		// TODO: components should get the page name directly
+		let i = $state.findIndex((x) => x.name == $page.data.page_name);
+
+		if (i != -1) {
+			for (const x of new URLSearchParams($state[i].search)) {
 				$page.url.searchParams.set(x[0], x[1]);
 			}
 			window.history.replaceState(history.state, '', $page.url.href);
