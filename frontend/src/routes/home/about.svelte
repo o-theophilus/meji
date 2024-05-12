9d270f297bc989791fcdda2da4b466c0a2758b4f
Theophilus <theophilus.ogbolu@gmail.com>
2024-05-12 11:43:14 +0100

    better about us

--- frontend/src/routes/home/about.svelte
+++ frontend/src/routes/home/about.svelte
@@ -17,36 +17,39 @@
 		</div>
 
 		<div class="row_2">
-			<div>
+			<div class="text">
 				<SVG icon="change" size="28" />
 				<br />
-				<span> Convenience Delivered </span>
-				<p>
-					We understand the value of time and convenience. That's why Meji offers you the ease of
-					shopping from anywhere, at any time. Our user-friendly platform allows you to browse and
-					purchase your desired items with just a few clicks. Plus, we ensure swift and reliable
-					delivery directly to your doorstep.
-				</p>
+				<span class="bold"> Convenience Delivered </span>
+
+				We understand the value of time and convenience. That's why Meji offers you the ease of
+				shopping from anywhere, at any time. Our user-friendly platform allows you to browse and
+				purchase your desired items with just a few clicks. Plus, we ensure swift and reliable
+				delivery directly to your doorstep.
 			</div>
-			<div>
+
+			<div class="vr" />
+
+			<div class="text">
 				<SVG icon="offer" size="28" />
 				<br />
-				<span> Secure Payment Solutions </span>
-				<p>
-					At Meji, your security is our top priority. With our trusted online shopping payment
-					methods, you can enjoy stress-free transactions. We prioritize the safety of your personal
-					and financial information, allowing you to shop with peace of mind.
-				</p>
+				<span class="bold"> Secure Payment Solutions </span>
+
+				At Meji, your security is our top priority. With our trusted online shopping payment
+				methods, you can enjoy stress-free transactions. We prioritize the safety of your personal
+				and financial information, allowing you to shop with peace of mind.
 			</div>
-			<div>
+
+			<div class="vr" />
+
+			<div class="text">
 				<SVG icon="time" size="28" />
 				<br />
-				<span> 24/7 Services </span>
-				<p>
-					We're dedicated to serving you around the clock. Shop online anytime, any day, and
-					experience our exceptional customer service. Our team is here to assist you with any
-					inquiries or support you may need throughout your shopping journey.
-				</p>
+				<span class="bold"> 24/7 Services </span>
+
+				We're dedicated to serving you around the clock. Shop online anytime, any day, and
+				experience our exceptional customer service. Our team is here to assist you with any
+				inquiries or support you may need throughout your shopping journey.
 			</div>
 		</div>
 	</section>
@@ -57,13 +60,10 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-
-		margin: var(--sp4) 0;
-		text-align: center;
 	}
 
 	.row_1 {
-		max-width: 300px;
+		margin-top: var(--sp2);
 	}
 
 	.row_2 {
@@ -72,29 +72,51 @@
 		flex-direction: column;
 		gap: var(--sp4);
 
-		margin: var(--sp4) 0;
+		margin-top: var(--sp5);
+		margin-bottom: var(--sp2);
+
 		fill: var(--cl1);
 	}
 
-	.row_2 div {
+	.row_2 .text {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-
-		max-width: 300px;
+		width: 100%;
 	}
 
-	span {
+	.bold {
 		font-weight: 700;
 		color: var(--ac1);
+		margin-bottom: var(--sp1);
+	}
+
+	.vr {
+		display: none;
+		flex-shrink: 0;
+
+		width: 2px;
+		background-color: var(--ac5);
 	}
 
 	@media screen and (min-width: 800px) {
+		section {
+			text-align: center;
+		}
+
+		.row_1 {
+			max-width: 600px;
+		}
 		.row_2 {
 			flex-direction: unset;
 		}
-		.row_2 div {
-			max-width: 200px;
+
+		.row_2 .text {
+			max-width: 300px;
+			align-items: center;
+		}
+
+		.vr {
+			display: unset;
 		}
 	}
 </style>
